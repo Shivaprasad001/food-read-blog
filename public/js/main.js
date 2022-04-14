263e2876ebc2b6270f47590fbfeef123d34ebaac
Shiva <shiva.geci@gmail.com>
2022-04-14 17:17:06 -0400

    public folder

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+// var wysiwyg = require('wysiwyg')
+ 
+// var editor = wysiwyg(document.querySelector('textarea'))
+ 
+// editor.selectAll()
+// editor.bold()
+// editor.color("pink")
+// editor.link("foobar.com")
+ 
+// editor.read();
+
+// console.log("working");
